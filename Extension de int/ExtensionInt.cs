882a4d5098c75896a3c4c8ba03e32095815dfb63
JorgeRZT <georgeforceone@gmail.com>
2017-08-03 12:15:19 +0200

    Agregar archivos de proyecto.

---
+++ Extension de int/ExtensionInt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extension_de_int
+{
+    static public class ExtensionInt
+    {
+        //Tiene que ser un método "public static" y recibir el parámetro "this"
+        //el cual hace referencia al "objeto" o "dato" desde el cual se invoca al método Between
+        public static bool Between(this int valor, int minValor, int maxValor)
+        {
+            return valor >= minValor && valor <= maxValor;
+        }
+    }
+}
---
+++ Extension de int/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Extension_de_int
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            //Extendemos el int añadiendo el método Between
+            a.Between(5, 15);
+        }
+    }
+
+
+}
